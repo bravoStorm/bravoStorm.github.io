7dff7b68a4465b764e66e8a3e92f2ac3db479dcd
bravoStorm <160444311+bravoStorm@users.noreply.github.com>
2024-05-25 01:14:17 -0300

    Add files via upload
    Adicionado verificações de campos de formulário

--- js/codes.js
+++ js/codes.js
@@ -70,10 +70,122 @@ function goToCotacao(){
     showCotacao();
 }
 
+function showEscola(){
+    window.location.href = "https://wa.me/5511991274324?text=Ol%C3%A1+Bravo+Storm%2C+gostaria+de+saber+mais+sobre+o+curso+de+Bombeiro+Civil.";
+}
+
 function goToNav(id){
     document.getElementById(id).scrollIntoView();
 
 }
 
+
+function verificaEmail(email){
+    let cont = email;
+    if( cont.indexOf("@") != 0 && cont.indexOf("@") != -1 && (cont.substring(cont.length-4) == ".com" || cont.substring(cont.length-3) == ".br" )){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function verificaCelular (celular) {
+    var regex1 = new RegExp('^((1[1-9])|([2-9][0-9]))((3[0-9]{3}[0-9]{4})|(9[0-9]{3}[0-9]{5}))$');
+    var regex2 = new RegExp('^\\([0-9]{2}\\)((3[0-9]{3}-[0-9]{4})|(9[0-9]{3}-[0-9]{5}))$');
+    return regex1.test(celular) || regex2.test(celular);
+}
+
+function verificaCep(cep){
+    cep.replace("-", "");
+    return cep.length == 8;
+}
+
+function verificaCamposCotacao(){
+    
+    if(!verificaEmail($(`input[name="emailCotacao"]`).val())){
+        alert("Email Inválido!")
+    }
+
+    if(!verificaCelular($(`input[name="celularCotacao"]`).val())){
+        alert("Celular inválido!");
+    }
+
+    else{
+       let dados = {
+        "tipoFormulario": "Cotação",
+        "serviço":  $(`select[name="servicoCotacao"]`).val(),
+        "nome": $(`input[name="nomeCotacao"]`).val(),
+        "email": $(`input[name="emailCotacao"]`).val(),
+        "celular": $(`input[name="celularCotacao"]`).val(),
+        "detalhes": $(`textarea[name="detCotacao"]`).val()
+       }
+
+       dados = JSON.stringify(dados);
+       
+       //Requisição para API
+       console.log(dados)
+       alert("Obrigado por nos contatar, entraremos em contato em breve!")
+
+    }
+}
+
+function verificaCamposFaleConosco(){
+    
+    if(!verificaEmail($(`input[name="emailFaleConosco"]`).val())){
+        alert("Email inválido");
+    }
+
+    else{
+        let dados = {
+            "tipoFormulario": "FaleConosco",
+            "assunto":  $(`input[name="assuntoFaleConosco"]`).val(),
+            "nome": $(`input[name="nomeFaleConosco"]`).val(),
+            "email": $(`input[name="emailFaleConosco"]`).val(),
+            "mensagem": $(`textarea[name="mensagemFaleConosco"]`).val()
+           }
+    
+        dados = JSON.stringify(dados);
+       
+        //Requisição para API
+        console.log(dados)
+        alert("Obrigado por nos contatar, entraremos em contato em breve!")
+    }
+}
+
+function verificaCamposTrabalheConosco(){
+    //profissao experiencia nome email celular cep mensagem
+
+    if(!verificaEmail($(`input[name="emailTrabalheConosco"]`).val())){
+        alert("Email Inválido!")
+    }
+
+    if(!verificaCelular($(`input[name="celularTrabalheConosco"]`).val())){
+        alert("Celular inválido!");
+    }
+
+    if(!verificaCep($(`input[name="CEPTrabalheConosco"]`).val())){
+        alert("CEP inválido!");
+    }
+
+    else{
+       let dados = {
+        "tipoFormulario": "TrabalheConosco",
+        "profissão":  $(`select[name="profissaoTrabalheConosco"]`).val(),
+        "experiencia":  $(`select[name="experienciaTrabalheConosco"]`).val(),
+        "nome": $(`input[name="nomeTrabalheConosco"]`).val(),
+        "email": $(`input[name="emailTrabalheConosco"]`).val(),
+        "celular": $(`input[name="celularTrabalheConosco"]`).val(),
+        "CEP": $(`input[name="CEPTrabalheConosco"]`).val(),
+        "mensagem": $(`textarea[name="mensagemTrabalheConosco"]`).val()
+       }
+
+       dados = JSON.stringify(dados);
+       
+       //Requisição para API
+       console.log(dados);
+       alert("Obrigado por nos contatar, entraremos em contato em breve!");
+    }
+}
 
 window.onload = () =>{ startCarrossel()};
